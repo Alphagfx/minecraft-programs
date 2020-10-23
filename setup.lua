e71f504ec11af36a054de5091bb9c6798930d5c2
Uladzislau Makarevich <vgarbat@gmail.com>
2020-10-24 02:14:31 +0300

    Fix setup script iteration

--- setup.lua
+++ setup.lua
@@ -1,6 +1,6 @@
 local shell = require("shell")
 
 local data = {"main.lua", "Transposer.lua", "Util.lua"}
-for file in pairs(data) do
+for idx, file in pairs(data) do
     shell.execute("wget -f https://tinyurl.com/y4ahygow/raw/" .. file .. " " .. file)
 end
