8f02c62b9790268150a2cdfd6cee446eca8c7e3a
Uladzislau Makarevich <vgarbat@gmail.com>
2020-12-22 00:51:30 +0300

    Update setup script

--- setup.lua
+++ setup.lua
@@ -7,6 +7,9 @@ local data = {
     {file = "Inventory.lua", url = gistUrl .. "/raw/Inventory.lua"},
     {file = "Util.lua", url = gistUrl .. "/raw/Util.lua"},
     {file = "Item.lua", url = gistUrl .. "/raw/Item.lua"},
+    {file = "Slot.lua", url = gistUrl .. "/raw/Slot.lua"},
+    {file = "Ticket.lua", url = gistUrl .. "/raw/Ticket.lua"},
+    {file = "Recipes.lua", url = gistUrl .. "/raw/Recipes.lua"},
     {file = "lib/log.lua", url = "https://raw.githubusercontent.com/rxi/log.lua/master/log.lua"}
 }
 
