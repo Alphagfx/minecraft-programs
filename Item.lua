f264dcc18ad5318296e79d3d7f3df893926dff4c
Uladzislau Makarevich <vgarbat@gmail.com>
2020-12-26 18:29:28 +0300

    Fix issue with equal
    If lua file does not return anything, it may be reevaluated and all his
    functions redefined. This leads to the issue with __eq method, because it is
    required for comparable objects to have the same ref! to __eq method.
    
    See getequalhandler (op1, op2) in Lua docs

--- Item.lua
+++ Item.lua
@@ -30,38 +30,13 @@ function Item:__eq(obj)
             and self.name == obj.name then
         return true
     else
-        if not self then
-            log.warn("self is false where self is", self)
-        end
-        if not type(obj) == "table" then
-            log.warn("type(obj) == \"table\" is false where type is", type(obj))
-        end
-        if not #self == #obj then
-            log.warn("#self == #obj is false where #self=", #self, "and #obj=", #obj)
-        end
-        if not self.damage == obj.damage then
-            log.warn("self.damage == obj.damage is false where self.damage=", self.damage, "and obj.damage=", obj.damage)
-        end
-        if not self.hasTag == obj.hasTag then
-            log.warn("self.hasTag == obj.hasTag is false where self.hasTag=", self.hasTag, "and obj.hasTag=", obj.hasTag)
-        end
-        if not self.label == obj.label then
-            log.warn("self.label == obj.label is false where self.label=", self.label, "and obj.label=", obj.label)
-        end
-        if not self.maxDamage == obj.maxDamage then
-            log.warn("self.maxDamage == obj.maxDamage is false where self.maxDamage=", self.maxDamage, "and obj.maxDamage=", obj.maxDamage)
-        end
-        if not self.name == obj.name then
-            log.warn("self.name == obj.name is false where self.name=", self.name, "and obj.name=", obj.name)
-        end
-
         return false
     end
 end
 
 function Item:__tostring()
     return string.format(
-            "{name=%s, label=%s, damage=%s, maxDamage=%s, hasTag=%s}",
+            "Item{name=%s, label=%s, damage=%s, maxDamage=%s, hasTag=%s}",
             self.name,
             self.label,
             self.damage,
@@ -69,3 +44,5 @@ function Item:__tostring()
             self.hasTag
     )
 end
+
+return Item
