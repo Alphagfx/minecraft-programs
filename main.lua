7fd4e5bc028bd425b2c0de8695a6824e3cb225bf
Uladzislau Makarevich <vgarbat@gmail.com>
2020-12-27 00:54:58 +0300

    Add buffer for complex crafting

--- main.lua
+++ main.lua
@@ -3,33 +3,28 @@ require("Util")
 local log = require("lib.log")
 local recipes = require("Recipes")
 
-function craft(recipe, source, target)
+function craft(recipe, source, buffer, target)
     log.debug("Check recipe", recipe.name, "can be crafted")
     if not source:contains(recipe.items) then
         log.debug("Recipe", recipe.name, ": source does not contain", recipe.items)
         return
     end
-    if not source:canFit(recipe.result) then
-        log.debug("Recipe", recipe.name, ": source can not fit result", recipe.result)
-        return
-    end
-    if not target:canFit(recipe.items) then
-        log.debug("Recipe", recipe.name, ": target can not fit", recipe.items)
+    if not buffer:canFit(recipe.items) then
+        log.debug("Recipe", recipe.name, ": buffer can not fit", recipe.items)
         return
     end
     if not target:canFit(recipe.result) then
-        log.debug("Recipe", recipe.name, ": target can not fit result", recipe.result)
+        log.debug("Recipe", recipe.name, ": source can not fit result", recipe.result)
         return
     end
 
     log.info("Crafting", recipe.name)
-    source:transferItemsTo(recipe.items, target)
+    source:transferItemsTo(recipe.items, buffer)
     log.info("Waiting for result...")
     while not target:contains(recipe.result) do
         log.trace("No result yet, sleeping for", 10, "seconds")
         os.sleep(10)
     end
-    target:transferItemsTo(recipe.result, source)
     log.info(recipe.name, "crafting complete")
 end
 
@@ -39,12 +34,14 @@ local transposer = Transposer:new(addr)
 local inventories = transposer:inventories()
 
 local source = inventories[1]
-local target = inventories[2]
+local buffer = inventories[2]
+local target = inventories[3]
 while true do
     log.info("Source", source, "\n", source:items())
+    log.info("Buffer", buffer, "\n", buffer:items())
     log.info("Target", target, "\n", target:items())
     for _, recipe in ipairs(recipes) do
-        craft(recipe, source, target)
+        craft(recipe, source, buffer, target)
     end
     log.debug("Sleeping...")
     os.sleep(10)
