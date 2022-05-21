c86c69ceb980147fb3938b198d94a94b8c45ad77
Uladzislau Makarevich <vgarbat@gmail.com>
2022-05-21 12:18:31 +0300

    Remove unused OS detect func

--- src/util.lua
+++ src/util.lua
@@ -6,27 +6,4 @@ function util.unload(module)
     package.loaded[module] = nil
 end
 
-function util.osName()
-    local status, result
-    status, result = pcall(util.osNameOC)
-    if status then
-        return result
-    end
-    status, result = pcall(util.osNameLinux)
-    if status then
-        return result
-    end
-    return nil
-end
-
-function util.osNameLinux()
-    os.execute("uname")
-    return "Linux"
-end
-
-function util.osNameOC()
-    require("shell")
-    return "OpenOS"
-end
-
 return util
